bb0e3d7d8622b60c12a4961c507babd772124495
Dustinyschild <dyelland@gmail.com>
2018-04-23 20:10:38 -0500

    cosntruction of 3 pokemon objects is functioning as expected

--- PokeApp/Program.cs
+++ PokeApp/Program.cs
@@ -13,18 +13,27 @@ namespace PokeApp
     {
         static Program()
         {
-            Pokemon Bulbasaur = new Pokemon(1, "bulbasaur", 1, 20);
+
         }
         static void Main(string[] args)
         {
+            Pokemon[] Pokemons = {
+                new Pokemon(1, "Bulbasaur", 1, 30),
+                new Pokemon(4, "Charmander", 1, 30),
+                new Pokemon(7, "Squirtle", 1, 30)
+            };
+
             Console.WriteLine("Hello World...");
             Console.ReadKey(true);
-            var Bulbasaur = GetPokemon("http://pokeapi.co/api/v2/pokemon/1");
-            Console.WriteLine(Bulbasaur.Id);
-            Console.WriteLine(Bulbasaur.Name);
-            Console.WriteLine(Bulbasaur.Types[0].Slot);
-            Console.WriteLine(Bulbasaur.Base_Experience);
-            Console.WriteLine(Bulbasaur.Moves[0]);
+            //var Bulbasaur = GetPokemon("http://pokeapi.co/api/v2/pokemon/1");
+            foreach(Pokemon Pokemon in Pokemons)
+            {
+                Console.WriteLine("Name: {0}, Index No.: {1}", Pokemon.Name, Pokemon.Id);
+            }
+            //Console.WriteLine(Bulbasaur.Name);
+            //Console.WriteLine(Bulbasaur.Types[0].Slot);
+            //Console.WriteLine(Bulbasaur.Base_Experience);
+            //Console.WriteLine(Bulbasaur.Moves[0]);
             Console.ReadKey(true);
         }
 
