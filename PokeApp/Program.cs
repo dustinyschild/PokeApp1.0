17dd42dea3e584114071c891ee7e82ef09467e2e
Dustinyschild <dyelland@gmail.com>
2018-04-23 20:12:54 -0500

    cleaning up unused api calling code

--- PokeApp/Program.cs
+++ PokeApp/Program.cs
@@ -23,35 +23,13 @@ namespace PokeApp
                 new Pokemon(7, "Squirtle", 1, 30)
             };
 
-            Console.WriteLine("Hello World...");
-            Console.ReadKey(true);
-            //var Bulbasaur = GetPokemon("http://pokeapi.co/api/v2/pokemon/1");
             foreach(Pokemon Pokemon in Pokemons)
             {
                 Console.WriteLine("Name: {0}, Index No.: {1}", Pokemon.Name, Pokemon.Id);
             }
-            //Console.WriteLine(Bulbasaur.Name);
-            //Console.WriteLine(Bulbasaur.Types[0].Slot);
-            //Console.WriteLine(Bulbasaur.Base_Experience);
-            //Console.WriteLine(Bulbasaur.Moves[0]);
-            Console.ReadKey(true);
-        }
 
-        public static Pokemon GetPokemon(string url)
-        {
-            HttpWebRequest webrequest = (HttpWebRequest)WebRequest.Create(url);
-            webrequest.Method = "GET";
-            webrequest.ContentType = "application/json";
-            //webrequest.Headers.Add("Username", "xyz");
-            //webrequest.Headers.Add("Password", "abc");
-            HttpWebResponse webresponse = (HttpWebResponse)webrequest.GetResponse();
-            Encoding enc = System.Text.Encoding.GetEncoding("utf-8");
-            StreamReader responseStream = new StreamReader(webresponse.GetResponseStream(), enc);
-            string result = string.Empty;
-            result = responseStream.ReadToEnd();
-            webresponse.Close();
-            Pokemon objectResult = JsonConvert.DeserializeObject<Pokemon>(result);
-            return objectResult;
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey(true);
         }
     }
 }
